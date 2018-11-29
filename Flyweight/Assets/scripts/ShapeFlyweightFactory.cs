2d0a1411349b1773671dbd11d7b62948d03c219f
bdodson81 <45437680+bdodson81@users.noreply.github.com>
2018-11-29 12:45:48 -0500

    Adding Flyweight
    Unity3d assests for Flyweight

---
+++ Flyweight/Assets/scripts/ShapeFlyweightFactory.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeFlyweightFactory
+{
+    private static List<GameObject> shapeList = new List<GameObject>();
+    private static List<string> hashvalues = new List<string>();
+    private static string hashstring;
+
+    public void CreateShape(string shape, string size, Color color)
+    {
+        hashstring = shape + size + color;
+        //Debug.Log("hashstring is " + hashstring);
+
+        int index;
+        GameObject newshape = null;
+        Vector3 vsize = new Vector3();
+
+        switch (size)
+        {
+            case "small":
+                vsize = new Vector3(1f, 1f, 1f);
+                break;
+            case "medium":
+                vsize = new Vector3(2f, 2f, 2f);
+                break;
+            case "large":
+                vsize = new Vector3(3f, 3f, 3f);
+                break;
+
+        }
+        //Debug.Log("count is " + shapeList.Count);
+
+        if (shapeList.Count != 0)
+        {
+            index = searchList(hashstring);
+
+            if (index >= 0)
+            {
+                //Positive value indicates that it found the hashstring -- means it already exists
+                if (index > 1)
+                {
+                    //Swap with item at index 0
+                    swapShapes(index, 0);
+                    //Debug.Log("found the shape at index = " + index);
+                }
+            }
+            else
+            {
+                //Negative value indicates that it didn't find the hashstring -- means it doesn't exist yet
+                if (shapeList.Count == 1)
+                {
+                    newshape = genPrimitive(shape);
+                    shapeList.Add(newshape);
+                    hashvalues.Add(hashstring);
+                    newshape.transform.position = new Vector3(5f, 2.5f, -45f);
+                    newshape.transform.localScale = vsize;
+                    newshape.GetComponent<Renderer>().material.color = color;
+                    //Debug.Log("Inserting second shape");
+
+                }
+                else
+                {
+                    // Add item to List and swap with object at index 0
+                    //Debug.Log("Did not find the shape: index = " + index);
+
+                    int newindex = shapeList.Count;
+                    newshape = genPrimitive(shape);
+                    shapeList.Add(newshape);
+                    hashvalues.Add(hashstring);
+                    int position_xmodifier;
+                    int position_zmodifier;
+                    int multiplier = (newindex - 2) / 5;
+
+                    position_xmodifier = (newindex - 2) - (multiplier * 5);
+                    position_zmodifier = (newindex -2) / 5;
+
+                    newshape.transform.position = new Vector3(-15f + position_xmodifier*5f, 4f, -5f - position_zmodifier*5f);
+
+                    newshape.transform.localScale = vsize;
+                    newshape.GetComponent<Renderer>().material.color = color;
+                    swapShapes(newindex, 0);
+
+                }
+
+            }
+        }
+        else
+        {
+            //List is empty
+            newshape = genPrimitive(shape);
+            shapeList.Add(newshape);
+            hashvalues.Add(hashstring);
+            newshape.transform.position = new Vector3(-1f, 2.5f, -45f);
+            newshape.transform.localScale = vsize;
+            newshape.GetComponent<Renderer>().material.color = color;
+            //Debug.Log("Inserting first shape");
+
+        }
+    }
+
+    public int searchList(string hashstring)  //Search the list for the hashstring.  Return negative if not found or return the index value
+    {
+
+        
+        return hashvalues.IndexOf(hashstring);
+    }
+
+    public GameObject genPrimitive(string shape) //Create the primitive and return the object
+    {
+        GameObject myshape = null;
+        switch (shape)
+        {
+            case "cube":
+                myshape = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                break;
+            case "sphere":
+                myshape = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                break;
+            case "cylinder":
+                myshape = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                break;
+        }
+        return myshape;
+    }
+
+    public void swapShapes(int index, int index2)
+    {
+        GameObject tempObj = new GameObject();
+        string tempHash;
+        Vector3 tempVector3 = new Vector3();
+
+        tempObj = shapeList[index2];
+        tempHash = hashvalues[index2];
+        tempVector3 = shapeList[0].transform.position;
+        //Debug.Log("tempHash is " + tempHash);
+
+        shapeList[index2] = shapeList[index];
+        hashvalues[index2] = hashvalues[index];
+        //Debug.Log("hashvalues[index2] is " + hashvalues[index2]);
+
+        shapeList[index] = tempObj;
+        shapeList[index].transform.position = shapeList[index2].transform.position;
+        hashvalues[index] = tempHash;
+        //Debug.Log("hashvalues[index] is " + hashvalues[index]);
+
+        shapeList[index2].transform.position = tempVector3;
+        //Debug.Log("swapping index = " + index + " with index2 = " + index2);
+
+    }
+
+}
---
+++ Flyweight/Assets/scripts/ShapeGameObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeGameObject : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        ShapeFlyweightFactory newshape = new ShapeFlyweightFactory();
+        newshape.CreateShape("cube", "small", Color.blue);
+        newshape.CreateShape("sphere", "small", Color.blue);
+        newshape.CreateShape("cylinder", "small", Color.blue);
+
+        newshape.CreateShape("cube", "medium", Color.blue);
+        newshape.CreateShape("sphere", "medium", Color.blue);
+        newshape.CreateShape("cylinder", "medium", Color.blue);
+
+        newshape.CreateShape("sphere", "large", Color.blue);
+        newshape.CreateShape("cylinder", "large", Color.blue);
+        newshape.CreateShape("cube", "large", Color.blue);
+
+        newshape.CreateShape("cube", "small", Color.red);
+        newshape.CreateShape("sphere", "small", Color.red);
+        newshape.CreateShape("cylinder", "small", Color.red);
+
+        newshape.CreateShape("cube", "medium", Color.red);
+        newshape.CreateShape("sphere", "medium", Color.red);
+        newshape.CreateShape("cylinder", "medium", Color.red);
+
+        newshape.CreateShape("sphere", "large", Color.red);
+        newshape.CreateShape("cylinder", "large", Color.red);
+        newshape.CreateShape("cube", "large", Color.red);
+
+        newshape.CreateShape("cube", "small", Color.green);
+        newshape.CreateShape("sphere", "small", Color.green);
+        newshape.CreateShape("cylinder", "small", Color.green);
+
+        newshape.CreateShape("cube", "medium", Color.green);
+        newshape.CreateShape("sphere", "medium", Color.green);
+        newshape.CreateShape("cylinder", "medium", Color.green);
+
+        newshape.CreateShape("sphere", "large", Color.green);
+        newshape.CreateShape("cylinder", "large", Color.green);
+        newshape.CreateShape("cube", "large", Color.green);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
